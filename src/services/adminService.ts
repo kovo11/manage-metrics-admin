b173fc376d98c2375f2507addc20963472c94a34
kovo11 <kovlam17>
2025-04-16 10:28:00 +0100

    done

--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -3,17 +3,9 @@
 // This maintains backward compatibility with existing imports
 
 export * from './authService';
-
-// Data services
-export * from './usersService';
-export * from './ordersService';
-export * from './ticketsService';
-export * from './messagesService';
-export * from './paymentsService';
-export * from './walletService';
-export * from './digitalProductsService';
-export * from './couponsService';
-export * from './homepageService';
+export * from './productService';
+export * from './userService';
+export * from './orderService';
 export * from './analyticsService';
 export * from './ticketService';
 export * from './messageService';
